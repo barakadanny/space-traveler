f771750dab46438510ad0b48cd78ac9500d3b0eb
barakadanny <53088629+barakadanny@users.noreply.github.com>
2022-09-26 14:01:20 +0300

    App container style Added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import Home from "./components/Home";
 
 function App() {
-  return <Home />;
+  return (
+    <div className="app-container">
+      <div className="content">
+        <Home />
+      </div>
+    </div>
+  );
 }
 
 export default App;

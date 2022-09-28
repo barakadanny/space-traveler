be5b71e158e7a9969cd46bfc9ff99eb776a34474
barakadanny <53088629+barakadanny@users.noreply.github.com>
2022-09-28 15:51:12 +0300

    Display rockets

--- src/components/Rockets/RocketsList.js
+++ src/components/Rockets/RocketsList.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
+import { loadRockets } from "../../store/rockets/rockets";
 import Rocket from "./Rocket";
 
 function RockectsList() {
+  const dispatch = useDispatch();
+  const rockets = useSelector((state) => state.rocket);
+
+  useEffect(() => {
+    dispatch(loadRockets());
+  }, [rockets, dispatch]);
+
   return (
     <div className="rockets-list">
-      <Rocket />
+      {rockets.map((rocket) => (
+        <Rocket />
+      ))}
     </div>
   );
 }
--- src/store/rockets/rockets.js
+++ src/store/rockets/rockets.js
@@ -1,37 +1,61 @@
-const LOAD_ROCKETS = "rockets/load";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+const LOAD_ROCKETS = "space-traveler/src/store/rockets/LOAD_ROCKETS";
 const RESERVE_ROCKET = "rockets/reserve";
 const CANCEL_ROCKET = "rockets/cancel";
 const BASE_URL = "https://api.spacexdata.com/v3/rockets";
 
-const displayRockets = (payload) => ({
-  type: LOAD_ROCKETS,
-  payload,
-});
+const init = [];
 
-export const fetchRockets = async (dispatch) => {
-  const res = await fetch(BASE_URL);
-  const rockets = await res.json();
-  console.log(rockets);
-  dispatch(
-    displayRockets(
-      rockets.map((rocket) => ({
-        id: rocket.rocket_id,
-        name: rocket.rocket_name,
-        desc: rocket.description,
-        img: rocket.flickr_images[0],
-      }))
-    )
-  );
-};
-
-const rocketReducer = (state = [], action) => {
+export default function rocketReducer(state = init, action) {
   switch (action.type) {
-    case LOAD_ROCKETS:
+    case "space-traveler/src/store/rockets/LOAD_ROCKETS/fulfilled":
       return action.payload;
 
     default:
       return state;
   }
-};
+}
 
-export default rocketReducer;
+export const loadRockets = createAsyncThunk(LOAD_ROCKETS, async () => {
+  const response = await fetch("https://api.spacexdata.com/v3/rockets");
+  const data = await response.json();
+  const rockets = Object.keys(data).map((key) => ({
+    ...data[key][0],
+    rocket_id: key,
+  }));
+  return rockets;
+});
+
+// const displayRockets = (payload) => ({
+//   type: LOAD_ROCKETS,
+//   payload,
+// });
+
+// export const fetchRockets = async (dispatch) => {
+//   const res = await fetch(BASE_URL);
+//   const rockets = await res.json();
+//   console.log(rockets);
+//   dispatch(
+//     displayRockets(
+//       rockets.map((rocket) => ({
+//         id: rocket.rocket_id,
+//         name: rocket.rocket_name,
+//         desc: rocket.description,
+//         img: rocket.flickr_images[0],
+//       }))
+//     )
+//   );
+// };
+
+// const rocketReducer = (state = [], action) => {
+//   switch (action.type) {
+//     case LOAD_ROCKETS:
+//       return action.payload;
+
+//     default:
+//       return state;
+//   }
+// };
+
+// export default rocketReducer;

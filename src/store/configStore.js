4c653c8bc8b85b83ec98059b2aabe0a95c7d5745
barakadanny <53088629+barakadanny@users.noreply.github.com>
2022-09-29 15:57:26 +0300

    Fix rocket reducer

--- src/store/configStore.js
+++ src/store/configStore.js
@@ -1,16 +1,17 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-import missionReducer from './misions/missions';
-import rocketReducer from './rockets/rockets';
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import missionReducer from "./misions/missions";
+import rocketReducer from "./rockets/rockets";
 
 const reducer = combineReducers({
-  rocket: rocketReducer,
+  rockets: rocketReducer,
   mission: missionReducer,
 });
 const store = configureStore({
   reducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger).concat(thunk),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(logger).concat(thunk),
 });
 
 export default store;

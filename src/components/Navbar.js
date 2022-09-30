3e43661fc7bf60f8256017dc9864d0f2418b505e
barakadanny <53088629+barakadanny@users.noreply.github.com>
2022-09-30 10:59:46 +0300

    Use NavLink for active

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,54 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Header() {
   const linkStyle = {
-    textDecoration: 'none',
-    color: '#121212',
+    textDecoration: "none",
+    color: "#121212",
+  };
+  const activeStyle = {
+    textDecoration: "underline",
+    color: "#0000FF",
   };
   return (
     <header>
       <nav className="navbar">
         <div className="navbar-brand d-flex">
-          <img className="logo" alt="logo" src={`${process.env.PUBLIC_URL}planet.png`} />
+          <img
+            className="logo"
+            alt="logo"
+            src={`${process.env.PUBLIC_URL}planet.png`}
+          />
           <h1 className="ms-3">Space Traveler&apos;s Hub</h1>
         </div>
-
       </nav>
       <ul className="navbar">
         <li className="nav-item">
-          <Link style={linkStyle} to="/">
+          <NavLink
+            to="/"
+            end
+            style={({ isActive }) => (isActive ? activeStyle : linkStyle)}
+          >
             Rockets
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link style={linkStyle} to="/missions">
+          <NavLink
+            to="/missions"
+            style={({ isActive }) => (isActive ? activeStyle : linkStyle)}
+          >
             Missions
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link style={linkStyle} to="/profile">
+          <NavLink
+            to="/profile"
+            style={({ isActive }) => (isActive ? activeStyle : linkStyle)}
+          >
             Profile
-          </Link>
+          </NavLink>
         </li>
       </ul>
     </header>

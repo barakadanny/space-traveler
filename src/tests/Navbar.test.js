f598814d4c3ed2bc41c6abf7ac207e1261495f2e
barakadanny <53088629+barakadanny@users.noreply.github.com>
2022-09-30 23:20:00 +0300

    Fix Linters 1

--- src/tests/Navbar.test.js
+++ src/tests/Navbar.test.js
@@ -1,49 +1,49 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import "@testing-library/jest-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import { Provider } from "react-redux";
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
-import Header from "../components/Navbar";
-import Missions from "../components/Missions/Missions";
-import Profile from "../components/profile/Profile";
+import Header from '../components/Navbar';
+import Missions from '../components/Missions/Missions';
+import Profile from '../components/profile/Profile';
 
-import store from "../store/configStore";
+import store from '../store/configStore';
 
-describe("Header", () => {
-  it("renders without crashing", () => {
+describe('Header', () => {
+  it('renders without crashing', () => {
     const header = render(
       //   <Provider store={store}>
       <Router>
         <Header />
-      </Router>
+      </Router>,
       //   </Provider>
     );
     expect(header).toMatchSnapshot();
   });
 });
 
-describe("Missions", () => {
-  it("renders without crashing", () => {
+describe('Missions', () => {
+  it('renders without crashing', () => {
     const mission = render(
       <Provider store={store}>
         <Router>
           <Missions />
         </Router>
-      </Provider>
+      </Provider>,
     );
     expect(mission).toMatchSnapshot();
   });
 });
 
-describe("Profile", () => {
-  it("renders without crashing", () => {
+describe('Profile', () => {
+  it('renders without crashing', () => {
     const profile = render(
       <Provider store={store}>
         <Router>
           <Profile />
         </Router>
-      </Provider>
+      </Provider>,
     );
     expect(profile).toMatchSnapshot();
   });
--- src/tests/Rockets.test.js
+++ src/tests/Rockets.test.js
@@ -1,11 +1,11 @@
-import renderer from "react-test-renderer";
-import { Provider } from "react-redux";
-import configureStore from "redux-mock-store";
-import Rockets from "../components/Rockets/RocketsList";
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import Rockets from '../components/Rockets/RocketsList';
 
 const mockStore = configureStore([]);
 
-describe("Rockets", () => {
+describe('Rockets', () => {
   let store;
   let component;
 
@@ -14,28 +14,28 @@ describe("Rockets", () => {
       rockets: [
         {
           id: 1,
-          name: "Falcon 1",
+          name: 'Falcon 1',
           description:
-            "First successful orbital launch vehicle in history, capable of delivering 13,450 kg to low Earth orbit. First successful orbital launch vehicle in history, capable of delivering 13,450 kg to low Earth orbit.",
-          image: "https://images2.imgbox.com/3c/0e/T8iJcSN3_o.png",
+            'First successful orbital launch vehicle in history, capable of delivering 13,450 kg to low Earth orbit. First successful orbital launch vehicle in history, capable of delivering 13,450 kg to low Earth orbit.',
+          image: 'https://images2.imgbox.com/3c/0e/T8iJcSN3_o.png',
         },
         {
           id: 2,
-          name: "Falcon 9",
+          name: 'Falcon 9',
           description:
-            "First successful orbital launch vehicle in history, capable of delivering 13,450 kg to low Earth orbit. First successful orbital launch vehicle in history, capable of delivering 13,450 kg to low Earth orbit.",
-          image: "https://images2.imgbox.com/3c/0e/T8iJcSN3_o.png",
+            'First successful orbital launch vehicle in history, capable of delivering 13,450 kg to low Earth orbit. First successful orbital launch vehicle in history, capable of delivering 13,450 kg to low Earth orbit.',
+          image: 'https://images2.imgbox.com/3c/0e/T8iJcSN3_o.png',
         },
       ],
     });
     component = renderer.create(
       <Provider store={store}>
         <Rockets />
-      </Provider>
+      </Provider>,
     );
   });
 
-  it("should render with given state from Redux store", () => {
+  it('should render with given state from Redux store', () => {
     expect(component.toJSON()).toMatchSnapshot();
   });
 });
